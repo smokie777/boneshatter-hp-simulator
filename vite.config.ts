7da93a4586c5a01db4ede8e40662fad868b1c8a9
smkycat <catherinehan714@gmail.com>
2025-06-06 06:32:28 -0600

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/boneshatter-hp-simulator/', // 👈 This is required for githug pages deployment
   plugins: [react()],
 })
